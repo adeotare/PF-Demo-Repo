92152d590ee6fac88a03ffa0d940736357f704a8
Abhay Deotare <adeotare@pfdrive.com>
2022-06-01 08:30:39 +0000

    Mohit and Sandeep Parihar

--- unpackaged/main/default/aura/ProjectListView/ProjectListViewController.js
+++ unpackaged/main/default/aura/ProjectListView/ProjectListViewController.js
@@ -1,22 +1,31 @@
 ({
 doinit : function(cmp, event, helper) {
-cmp.set('v.Columns', [
- { label: ' Name', fieldName: 'Name', type: 'text' } ,
-{ label: 'Project Name', fieldName: 'Project_Name__c ', type: 'text' },
+     
+cmp.set('v.Columns', [  {label: 'Account Name', fieldName: 'linkName', type: 'url', 
+        typeAttributes: {label: { fieldName: 'Name' }, target: '_blank'}},
+
+ { label: 'Project Name', fieldName: 'Project_Name__c ', type: 'text' },
 { label: 'Project Type', fieldName: 'Project_Type__c', type: 'text' },
 { label: 'Project Visibility', fieldName: 'Project_visibility__c', type: 'text' },
 { label: 'Product ', fieldName: 'Product__c ', type: 'text' }
 
 ]);
+    
 var action = cmp.get("c.fetchDetails");
 action.setParams({
 }); 
     action.setCallback(this, function(response){
 var state = response.getState();
 if (state === "SUCCESS") {
-cmp.set("v.ProjectList", response.getReturnValue());
+        var records =response.getReturnValue();
+                records.forEach(function(record){
+                    record.linkName = '/'+record.Id;
+                }); 
+cmp.set("v.ProjectList", records);
 }
 });
 $A.enqueueAction(action);
+    
+   
 }
 })
